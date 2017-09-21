b77425be9a0d139883044140457f1f2626e733dd
julio.cesar <julio.cesar@soaexpert.com.br>
2017-09-20 23:06:57 -0300

    Added backend graphql.js

---
+++ graphql-js/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// This package automatically parses JSON requests.
+const bodyParser = require('body-parser');
+
+// This package will handle GraphQL server requests and responses
+// for you, based on your schema.
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+
+const schema = require('./schema');
+
+var app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Hackernews GraphQL server running on port ${PORT}.`)
+});
