b7ef9903ffb2648038c5a15dae3b7a073ab5148d
julio.cesar <julio.cesar@soaexpert.com.br>
2017-09-21 22:37:31 -0300

    Added connection on graphql.js

--- graphql-js/src/index.js
+++ graphql-js/src/index.js
@@ -1,21 +1,29 @@
 const express = require('express');
-
-// This package automatically parses JSON requests.
 const bodyParser = require('body-parser');
-
-// This package will handle GraphQL server requests and responses
-// for you, based on your schema.
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-
+const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
 const schema = require('./schema');
 
-var app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-}));
+// 1
+const connectMongo = require('./mongo-connector');
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Hackernews GraphQL server running on port ${PORT}.`)
-});
+// 2
+const start = async () => {
+  // 3
+  const mongo = await connectMongo();
+  var app = express();
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    context: {mongo}, // 4
+    schema
+  }));
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+  }));
+
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Hackernews GraphQL server running on port ${PORT}.`)
+  });
+};
+
+// 5
+start();
