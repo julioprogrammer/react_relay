d47edda74c0a496936082d8af49650d230c7fec5
julio cesar <julio.cesar@soaexpert.com.br>
2017-09-23 16:53:49 -0300

    Added dataloaders and subscriptions

--- graphql-js/src/index.js
+++ graphql-js/src/index.js
@@ -3,9 +3,17 @@ const bodyParser = require('body-parser');
 const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
 const schema = require('./schema');
 const {authenticate} = require('./authentication');
+const buildDataloaders = require('./dataloaders');
+const formatError = require('./formatError');
+const { execute, subscribe } = require('graphql');
+const { createServer } = require('http');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
+
 
 // 1
 const connectMongo = require('./mongo-connector');
+
+const PORT = 3000;
 
 // 2
 const start = async () => {
@@ -15,20 +23,31 @@ const start = async () => {
  const buildOptions = async (req, res) => {
     const user = await authenticate(req, mongo.Users);
     return {
-      context: {mongo, user}, // This context object is passed to all resolvers.
+      context: {
+        dataloaders: buildDataloaders(mongo),
+        mongo, 
+        user
+      }, // This context object is passed to all resolvers.
+      formatError,
       schema,
     };
   };
   app.use('/graphql', bodyParser.json(), graphqlExpress(buildOptions));
   app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql',
-    passHeader: `'Authorization': 'bearer token-foo@bar.com'`,
+    passHeader: `'Authorization': 'bearer token-julioreiddt@hotmail.com'`,
+    subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`,
   }));
 
-  const PORT = 3000;
-  app.listen(PORT, () => {
+  const server = createServer(app);
+  server.listen(PORT, () => {
+    SubscriptionServer.create(
+      { execute, subscribe, schema },
+      { server, path: '/subscriptions' },
+    );
     console.log(`Hackernews GraphQL server running on port ${PORT}.`)
   });
+
 };
 
 // 5
--- graphql-js/src/schema/resolvers.js
+++ graphql-js/src/schema/resolvers.js
@@ -1,44 +1,142 @@
+const { ObjectID } = require('mongodb');
+const pubsub = require('../pubsub');
+
+const { URL } = require('url');
+
+function assertValidLink({ url }) {
+  try {
+    new URL(url);
+  } catch (error) {
+    throw new Error('Link validation error: invalid url.');
+  }
+}
+
+class ValidationError extends Error {
+  constructor(message, field) {
+    super(message);
+    this.field = field;
+  }
+}
+
+function assertValidLink({ url }) {
+  try {
+    new URL(url);
+  } catch (error) {
+    throw new ValidationError('Link validation error: invalid url.', 'url');
+  }
+}
+
 module.exports = {
   Query: {
+
     allLinks: async (root, data, {mongo: {Links}}) => { // 1
       return await Links.find({}).toArray(); // 2
     },
+
+    allUsers: async (root, data, {mongo: {Users}}) => {
+      return await Users.find({}).toArray();
+    }
+
   },
 
   Mutation: {
-    createLink: async (root, data, {mongo: {Links}, user}) => {
-        const newLink = Object.assign({postedById: user && user._id}, data);
-        const response = await Links.insert(newLink);
-        return Object.assign({id: response.insertedIds[0]}, newLink);
+
+    createLink: async (root, data, { mongo: { Links }, user }) => {
+      assertValidLink(data);
+      const newLink = Object.assign({ postedById: user && user._id }, data)
+      const response = await Links.insert(newLink);
+
+      newLink.id = response.insertedIds[0]
+      pubsub.publish('Link', { Link: { mutation: 'CREATED', node: newLink } });
+
+      return newLink;
     },
+
     createUser: async (root, data, {mongo: {Users}}) => {
-      // You need to convert the given arguments into the format for the
-      // `User` type, grabbing email and password from the "authProvider".
       const newUser = {
           name: data.name,
           email: data.authProvider.email.email,
           password: data.authProvider.email.password,
       };
       const response = await Users.insert(newUser);
-      return Object.assign({id: response.insertedIds[0]}, newUser);
+
+      newUser.id = response.insertedIds[0]
+      pubsub.publish('User', { User: { mutation: 'CREATED', node: newUser } });
+
+      return newUser;
     },
+
     signinUser: async (root, data, {mongo: {Users}}) => {
       const user = await Users.findOne({email: data.email.email});
       if (data.email.password === user.password) {
         return {token: `token-${user.email}`, user};
       }
     },
+
+    createVote: async (root, data, { mongo: { Votes }, user }) => {
+      const newVote = {
+        userId: user && user._id,
+        linkId: new ObjectID(data.linkId),
+      };
+      const response = await Votes.insert(newVote);
+      
+      newVote.id = response.insertedIds[0]
+      pubsub.publish('Vote', { Vote: { mutation: 'CREATED', node: { newVote } } });
+
+      return newVote;
+    },
+
+  },
+
+  Subscription: {
+
+    Link: {
+      subscribe: () => pubsub.asyncIterator('Link'),
+    },
+
+    Vote: {
+      subscribe: () => pubsub.asyncIterator('Vote'),
+    },
+
+    User: {
+      subscribe: () => pubsub.asyncIterator('User'),
+    },
+
   },
 
   User: {
     // Convert the "_id" field from MongoDB to "id" from the schema.
     id: root => root._id || root.id,
+    votes: async ({ _id }, data, { mongo: { Votes } }) => {
+      return await Votes.find({ userId: _id }).toArray();
+    },
   },
 
   Link: {
     id: root => root._id || root.id, // 5
     postedBy: async ({postedById}, data, {mongo: {Users}}) => {
         return await Users.findOne({_id: postedById});
+    },
+    votes: async ({ _id }, data, { mongo: { Votes } }) => {
+      return await Votes.find({ linkId: _id }).toArray();
+    },
+    postedBy: async ({ postedById }, data, { dataloaders: { userLoader } }) => {
+      return await userLoader.load(postedById);
+    },
+  },
+
+  Vote: {
+    id: root => root._id || root.id,
+
+    user: async ({ userId }, data, { mongo: { Users } }) => {
+      return await Users.findOne({ _id: userId });
+    },
+
+    link: async ({ linkId }, data, { mongo: { Links } }) => {
+      return await Links.findOne({ _id: linkId });
+    },
+    user: async ({ userId }, data, { dataloaders: { userLoader } }) => {
+      return await userLoader.load(userId);
     },
   },
 };
